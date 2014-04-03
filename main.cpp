3c41bfc02bca4b839bd4110913ebe73e614a4313
Joe Ferner <joe@fernsroth.com>
2014-04-03 19:56:32 -0400

    added calculation for mouse location

--- main.cpp
+++ main.cpp
@@ -2,13 +2,20 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/video/background_segm.hpp>
 #include <vector>
+#include <math.h>
 #include "input.h"
 
-#define WINDOW_NAME   "Camera Multi-touch"
-#define CAMERA_COUNT  1
-#define BACKGROUND_SUBTRACTOR_REFRESH 0.001
-#define WINDOW_IMG_HEIGHT 768
-#define WINDOW_IMG_WIDTH 1024
+#define WINDOW_NAME                    "Camera Multi-touch"
+#define CAMERA_COUNT                   2
+#define PI                             3.1415
+#define BACKGROUND_SUBTRACTOR_REFRESH  0.001
+#define PREVIEW_WINDOW_WIDTH           1024
+#define PREVIEW_WINDOW_HEIGHT          768
+#define CAMERA_DISTANCE                2000       /* calibrate to the x distance of the two cameras */
+#define CAMERA_DISTANCE_FROM_SCREEN_X  200        /* calibrate to the x distance from top of screen to camera */
+#define CAMERA_DISTANCE_FROM_SCREEN_Y  200        /* calibrate to the y distance from top of screen to camera */
+#define CAMERA_WIDTH                   1280       /* set to camera x resolution */
+#define CAMERA_FOV                     1.20427718 /* 69 degrees */
 
 CvCapture* capture[CAMERA_COUNT];
 InputContext* inputContext;
@@ -16,8 +23,9 @@ cv::Mat backgroundSubtractorOutput[CAMERA_COUNT];
 cv::Mat* backgroundSubtractorColorOutput[CAMERA_COUNT];
 cv::Ptr<cv::BackgroundSubtractor> backgroundSubtractor[CAMERA_COUNT];
 cv::Ptr<cv::FeatureDetector> blobDetector[CAMERA_COUNT];
-cv::Mat windowImg(WINDOW_IMG_HEIGHT + 1, WINDOW_IMG_WIDTH + 1, CV_8UC3 /*CV_8UC1*/);
+cv::Mat windowImg(PREVIEW_WINDOW_HEIGHT + 1, PREVIEW_WINDOW_WIDTH + 1, CV_8UC3 /*CV_8UC1*/);
 cv::Rect* cropRects[CAMERA_COUNT];
+cv::KeyPoint detectedBlobCoords[CAMERA_COUNT];
 
 int captureInput = 0;
 
@@ -28,6 +36,7 @@ void releaseCameraCapture();
 void initBackgroundSubtractors();
 void initDetectors();
 void displayCaptures(int cameraIdx, std::vector<cv::KeyPoint> &keypoints);
+void calculateLocations();
 
 int main(int argc, char** argv) {
   if (captureInput) {
@@ -38,7 +47,7 @@ int main(int argc, char** argv) {
   initDetectors();
 
   for (int i = 0; i < CAMERA_COUNT; i++) {
-    cropRects[i] = new cv::Rect(0, 500, 1280, 100);
+    cropRects[i] = new cv::Rect(0, 500, CAMERA_WIDTH, 100);
     backgroundSubtractorColorOutput[i] = NULL;
   }
 
@@ -66,11 +75,11 @@ int main(int argc, char** argv) {
 
         displayCaptures(i, keypoints);
 
-        cv::Mat targetColorDisplayImg(windowImg, cv::Rect(0, y, WINDOW_IMG_WIDTH, cropRects[i]->height));
+        cv::Mat targetColorDisplayImg(windowImg, cv::Rect(0, y, PREVIEW_WINDOW_WIDTH, cropRects[i]->height));
         y += cropRects[i]->height;
         cv::resize(croppedImg, targetColorDisplayImg, targetColorDisplayImg.size());
 
-        cv::Mat targetDisplayImg(windowImg, cv::Rect(0, y, WINDOW_IMG_WIDTH, cropRects[i]->height));
+        cv::Mat targetDisplayImg(windowImg, cv::Rect(0, y, PREVIEW_WINDOW_WIDTH, cropRects[i]->height));
         y += cropRects[i]->height;
         cv::resize(*backgroundSubtractorColorOutput[i], targetDisplayImg, targetDisplayImg.size());
       } catch (cv::Exception e) {
@@ -79,11 +88,12 @@ int main(int argc, char** argv) {
     }
 
     cv::imshow(WINDOW_NAME, windowImg);
+    calculateLocations();
 
     if (cv::waitKey(10) >= 0) {
       break;
     }
-  }
+  } // END while(1))
 
   releaseCameraCapture();
 
@@ -100,16 +110,51 @@ void displayCaptures(int cameraIdx, std::vector<cv::KeyPoint> &keypoints) {
   if (keypoints.size() > 0) {
     float x = keypoints[0].pt.x;
     float y = keypoints[0].pt.y;
-    if (captureInput) {
-      inputMouseMove(inputContext, x, y);
-    }
+    detectedBlobCoords[cameraIdx].pt.x = x;
+    detectedBlobCoords[cameraIdx].pt.y = y;
+    printf("%f, %f\n", x, y);
+  } else {
+    detectedBlobCoords[cameraIdx].pt.x = -1;
+    detectedBlobCoords[cameraIdx].pt.y = -1;
   }
   for (int j = 0; j < keypoints.size(); j++) {
     float x = keypoints[j].pt.x;
     float y = keypoints[j].pt.y;
-    printf("%f, %f\n", x, y);
     cv::line(*backgroundSubtractorColorOutput[cameraIdx], cv::Point(x, y - 10), cv::Point(x, y + 10), cv::Scalar(0, 255, 0));
     cv::line(*backgroundSubtractorColorOutput[cameraIdx], cv::Point(x - 10, y), cv::Point(x + 10, y), cv::Scalar(0, 255, 0));
+  }
+}
+
+// see camera_layout
+void calculateLocations() {
+  if (CAMERA_COUNT != 2) {
+    return;
+  }
+  if (detectedBlobCoords[0].pt.x < 0 || detectedBlobCoords[1].pt.x < 0) {
+    return;
+  }
+
+  float theta_A = detectedBlobCoords[0].pt.x / (float) CAMERA_WIDTH * CAMERA_FOV;
+  float theta_B = detectedBlobCoords[1].pt.x / (float) CAMERA_WIDTH * CAMERA_FOV;
+  float theta_P = PI - theta_A - theta_B;
+
+  float d_A = CAMERA_DISTANCE * sin(theta_A) / sin(theta_P);
+  float d_B = CAMERA_DISTANCE * sin(theta_B) / sin(theta_P);
+
+  float x_prime = cos(theta_A) * d_A;
+  float y_prime = sin(theta_A) * d_A;
+
+  float x = x_prime - CAMERA_DISTANCE_FROM_SCREEN_X;
+  float y = y_prime - CAMERA_DISTANCE_FROM_SCREEN_Y;
+
+  printf(
+          "x=%d, y=%d (theta_A=%.2f, theta_B=%.2f, theta_P=%.2f, d_A=%.2f, d_B=%.2f, x_prime=%d, y_prime=%d)\n",
+          x, y,
+          theta_A, theta_B, theta_P,
+          d_A, d_B,
+          x_prime, y_prime);
+  if (captureInput) {
+    inputMouseMove(inputContext, x, y);
   }
 }
 
